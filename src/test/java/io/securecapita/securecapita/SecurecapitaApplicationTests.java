12eafa9816132441e281f95b42a103cac22778b2
AbenezerM <abenezermulugeta41@gmail.com>
2025-02-08 19:06:45 -0500

    Project init, built users db schema

---
+++ src/test/java/io/securecapita/securecapita/SecurecapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.securecapita.securecapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
